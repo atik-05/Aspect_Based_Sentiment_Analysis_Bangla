996406ecd09bd49a287e80141ff9653b97dfde6c
Atik Rahman <bsse05217260>
2018-02-12 19:01:03 +0600

    bangla wv modified and dataset testing with popular algorithm completed

---
+++ english_restaurant_popular_classifiers.py
@@ -0,0 +1,110 @@
+import pandas as pd
+import numpy as np
+import Text_preprocessor
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.svm import SVC
+from sklearn.metrics import classification_report
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+
+
+
+review_dataset = 'restaurant.csv'
+
+number_of_category = 5
+train_percentage = 0.8
+
+def evaluate_model(target_true,target_predicted):
+    print (classification_report(target_true,target_predicted))
+    print ("The accuracy score is {:.2%}".format(accuracy_score(target_true,target_predicted)))
+
+def get_tfidf_value(data):
+    count_vectorizer = CountVectorizer(binary='true', stop_words='english')
+    data = count_vectorizer.fit_transform(data)
+    tfidf_data = TfidfTransformer(use_idf=False).fit_transform(data).toarray()
+    return tfidf_data
+
+def get_data_and_lebel():
+    reviews = pd.read_csv(review_dataset, encoding='ISO-8859-1')
+
+    x = reviews['text'].values
+    y = reviews['category'].values
+
+    my_set = list(sorted(set(y)))
+    label = np.zeros(len(my_set))
+    # if y[1] == 'food':
+    #     ind = my_set.index('food')
+    #     label[ind] = 1
+
+    prev_rev = ''
+    prev_lebel = []
+    my_review = []
+    my_label = []
+    i = 0
+    for review in x:
+
+        if review == prev_rev:
+            label = prev_lebel
+            for index, category in enumerate(my_set):
+                if y[i] == category:
+                    label[index] = 1
+            my_label[-1] = label
+            i += 1
+            prev_lebel = label
+            continue
+
+        my_review.append(review)
+        label = np.zeros(len(my_set))
+
+        for index, category in enumerate(my_set):
+            if y[i] == category:
+                label[index] = 1
+
+        my_label.append(label)
+        i += 1
+        prev_rev = review
+        prev_lebel = label
+    return my_review, my_label
+
+x, y = get_data_and_lebel()
+# mlb = MultiLabelBinarizer()
+# y_enc = mlb.fit_transform(y)
+x = [Text_preprocessor.clean_english_string(text) for text in x]
+x = get_tfidf_value(x)
+y = np.array(y)
+y = y.astype(int)
+
+train_len = int(len(x) * train_percentage)
+x_train = x[:train_len]
+y_train = y[:train_len]
+x_test = x[train_len:]
+y_test = y[train_len:]
+print('train test ready')
+
+from sklearn.preprocessing import MultiLabelBinarizer
+# Y = MultiLabelBinarizer().fit_transform(yvalues)
+
+clf = OneVsRestClassifier(SVC(kernel='linear'))
+clf.fit(x_train, y_train)
+predictions = clf.predict(x_test)
+
+evaluate_model(y_test, predictions)
+
+print('going well ...')
+
+print('for random forest....')
+cls = RandomForestClassifier()
+cls.fit(x_train, y_train)
+pred = cls.predict(x_test)
+evaluate_model(y_test, pred)
+
+print('KNN...')
+cls = KNeighborsClassifier(n_neighbors=3)
+cls.fit(x_train, y_train)
+pred = cls.predict(x_test)
+evaluate_model(y_test, pred)
+
--- word2vec_model_bangla.py
+++ word2vec_model_bangla.py
@@ -3,7 +3,27 @@ from gensim import models
 from nltk.tokenize import word_tokenize
 import re
 import string
+import logging
+from datetime import datetime
 
+logging.basicConfig(filename='data/word_embeddings/log_of_bangla_wv.txt', level=logging.INFO)
+
+
+
+
+def get_all_bangla_text():
+    bangla_corpus = []
+    for i in range(3, 8):
+        file_name = 'data/bangla_newspaper/paper_201' + str(i) + '.txt'
+        with open(file_name, 'r', encoding='utf8') as file:
+            bangla_corpus += file.readlines()
+    return bangla_corpus
+
+def count_word(text_list):
+    counter = 0
+    for line in text_list:
+        counter += len(line.split())
+    return counter
 
 def clean_str(text):
     text = re.sub("\d", "", text)           #remove decimal number
@@ -13,17 +33,24 @@ def clean_str(text):
     text = text.translate(translator)
     return text
 
-# bangla_text = list(open('paper.txt', encoding='utf8').readlines())
-#
-#
-# text = [clean_str(i) for i in bangla_text]
-# text = [word_tokenize(i) for i in text]
-#
-# model = models.Word2Vec(text, size=50, window=3, min_count=2, workers=4)
-# model.wv.save_word2vec_format('bangla_word2vec.txt', binary=False)
-# print('bangla model created')
 
-model = models.KeyedVectors.load_word2vec_format('bangla_word2vec.txt', binary=False)
-print('word2vec model loaded')
-word_vec = model.most_similar('চেয়ারম্যান')
-print(word_vec)
+
+bangla_corpus = get_all_bangla_text()
+bangla_corpus = [clean_str(i) for i in bangla_corpus]
+number_of_words = count_word(bangla_corpus)
+print('total number of words: ', number_of_words)
+
+text = [word_tokenize(i) for i in bangla_corpus]
+
+model = models.Word2Vec(text, size=100, window=5, min_count=4, sg=1)
+model.wv.save_word2vec_format('data/word_embeddings/bangla_wv_sg_window5_min4.txt', binary=False)
+print('bangla model created')
+
+logging.info('present word vectors is created named bangla_wv_cbow_window5_min4 at %s...' %datetime.now() )
+
+print('its done...')
+
+# model = models.KeyedVectors.load_word2vec_format('bangla_word2vec.txt', binary=False)
+# print('word2vec model loaded')
+# word_vec = model.most_similar('চেয়ারম্যান')
+# print(word_vec)
